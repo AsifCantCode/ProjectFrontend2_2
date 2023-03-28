27d0ea4eba414a6c05a9aaebfd9b131b367dce8a
Asif Abrar <Asif141100@gmail.com>
2023-03-29 01:49:24 +0600

    Email pathaisi

--- src/main/java/com/example/projectfrontend2_2/email.java
+++ src/main/java/com/example/projectfrontend2_2/email.java
@@ -31,50 +31,61 @@ public class email {
     private  TextField txtsub;
 
     public void sendMail(ActionEvent event) throws IOException, InterruptedException{
-        System.out.println(txtEmail.getText());
+            System.out.println(txtEmail.getText());
             String to = "asif141100@gmail.com";
-            String from = "asifabrar@iut-dhaka.edi";
+            String from = "asifabrar@iut-dhaka.edu";
             String host = "localhost";
             String port = "25";
 
-            final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-
+            //Setting Properties
             Properties props = System.getProperties();
-            props.setProperty("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-            props.setProperty("mail.smtp.socketFactory.fallback", "false");
-            props.setProperty("mail.smtp.port", "465");
-            props.setProperty("mail.smtp.socketFactory.port", "465");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.debug", "true");
-            props.put("mail.store.protocol", "pop3");
-            props.put("mail.transport.protocol", "smtp");
+            propertySetter(props);
+
             final String username = "asifabrar@iut-dhaka.edu";//
             final String password = "";
-            try{
-                Session session = Session.getInstance(props,
-                        new Authenticator(){
-                            protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(username, password);
-                            }});
+            instaSendMail(username, password, txtEmail.getText(), txtsub.getText(), txtmsg.getText(), props);
 
-                Message msg = new MimeMessage(session);
+    }
+
+    public void propertySetter(Properties props){
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.store.protocol", "pop3");
+        props.put("mail.transport.protocol", "smtp");
+    }
+    public void instaSendMail(String username, String pass, String toMail, String sub, String message, Properties props){
+        try{
+            Session session = Session.getInstance(props,
+                    new Authenticator(){
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, pass);
+                        }});
 
 
-                msg.setFrom(new InternetAddress(txtEmail.getText()));
-                msg.setRecipients(Message.RecipientType.TO,
-                        InternetAddress.parse(txtEmail.getText(),false));
-                msg.setSubject(txtsub.getText());
-                msg.setText(txtmsg.getText());
+
+            Message msg = new MimeMessage(session);
+
+
+            msg.setFrom(new InternetAddress(toMail));
+            msg.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toMail,false));
+            msg.setSubject(sub);
+            msg.setText(message);
 
 
 
 
 
-                msg.setSentDate(new Date());
-                Transport.send(msg);
-                JOptionPane.showMessageDialog(null, "Sent");
-            }catch (MessagingException e){ System.out.println("Erreur d'envoi, cause: " + e);}
+            msg.setSentDate(new Date());
+            Transport.send(msg);
+            JOptionPane.showMessageDialog(null, "Sent");
+        }catch (MessagingException e){ System.out.println("Error, cause: " + e);}
     }
     private static String getExceptionMessage(Exception e) {
         StringWriter sw = new StringWriter();
