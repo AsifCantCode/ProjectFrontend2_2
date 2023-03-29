354e58af639cbdd0de3a283a9b162e9bf8d15af5
Asif Abrar <Asif141100@gmail.com>
2023-03-30 02:56:05 +0600

    Back button done

--- src/main/java/com/example/projectfrontend2_2/Classroom/CreateAssignment.java
+++ src/main/java/com/example/projectfrontend2_2/Classroom/CreateAssignment.java
@@ -1,12 +1,20 @@
 package com.example.projectfrontend2_2.Classroom;
 
+import com.example.projectfrontend2_2.Attendance;
+import com.example.projectfrontend2_2.HelloApplication;
+import com.example.projectfrontend2_2.core2;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -16,6 +24,9 @@ public class CreateAssignment {
 
     @FXML
     private ListView listview;
+
+    @FXML
+    private Button back;
 
     public void ButtonAction(ActionEvent event){
         FileChooser fc = new FileChooser();
@@ -32,5 +43,18 @@ public class CreateAssignment {
         }else{
             System.out.println("File Not Valid!");
         }
+    }
+
+    @FXML
+    public void GoBack(ActionEvent event) throws IOException, InterruptedException {
+        Node root = (Node) event.getSource();
+        Stage myStage = (Stage) root.getScene().getWindow();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scrolscrene2.fxml"));
+        Scene subtractionScene = new Scene(fxmlLoader.load());
+        core2 obj = fxmlLoader.getController();
+
+        myStage.setScene(subtractionScene);
+        myStage.show();
     }
 }
